03fcf348dd699ea98cb7365dc36137f690efbeeb
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-02-06 14:53:54 +0530

    sm8150-common: build QTI Thermal HAL
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- common.mk
+++ common.mk
@@ -483,6 +483,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus_msmnile
