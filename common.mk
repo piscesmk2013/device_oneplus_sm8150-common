e02dc1329ca1c53dc4fedc74357c05e96a850a00
baalajimaestro <me@baalajimaestro.me>
2021-03-21 09:49:52 +0000

    sm8150-common: power: Import powerhint from  LA.UM.9.1.r1-09100-SMxxx0.0
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- common.mk
+++ common.mk
@@ -414,6 +414,8 @@ PRODUCT_BOOT_JARS += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Protobuf
 PRODUCT_PACKAGES += \
