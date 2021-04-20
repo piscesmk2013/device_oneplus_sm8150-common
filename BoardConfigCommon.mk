a78794aead7a9661542114e1fe98e18562291ec9
piscesmk2013 <piscesmk2013@gmail.com>
2021-04-20 20:24:59 +0800

    sm8150-common: Use Gluch kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CONFIG := gulch_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
 
 # Platform
