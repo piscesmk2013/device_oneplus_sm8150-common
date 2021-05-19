ed1c1a55f62b81b5e5e8e9a8c9d185c4ddea4610
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-05-19 22:56:22 +0300

    sm8150-common: Switch to prebuilt camera provider
    * Opensource one not working properly with OnePlusCamera.

--- common.mk
+++ common.mk
@@ -153,12 +153,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl:64 \
-    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     vendor.oneplus.hardware.camera@1.0.vendor:64 \
-    vendor.oneplus.hardware.CameraMDMHIDL@1.0.vendor:64 \
-    vendor.qti.hardware.camera.device@1.0.vendor:64
+    vendor.oneplus.hardware.CameraMDMHIDL@1.0.vendor:64
 
 # Common init scripts
 PRODUCT_PACKAGES += \
