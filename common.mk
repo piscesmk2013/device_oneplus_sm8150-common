8861d86389b0dad9d46fce498f2671308f812e75
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-20 16:58:41 +0000

    sm8150-common: drop unnecessary trailing \
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
     libspkrprot \
     libtinycompress \
     libvolumelistener \
-    tinymix \
+    tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
