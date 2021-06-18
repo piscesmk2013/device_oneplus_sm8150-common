f577a751bc5c1f37de0b174df5baa90d0d8e1ac8
Pranav Vashi <neobuddy89@gmail.com>
2021-06-19 03:31:46 +0530

    sm8150-common: Compile missing libcamera2ndk_vendor
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -104,6 +104,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    libcamera2ndk_vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
