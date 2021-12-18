481bfb5c2ebdc9220438fd17aa0dc71eba47257d
Amit Pundir <amit.pundir@linaro.org>
2021-12-19 00:43:22 +0800

    sm8150-common: Copy mediaswcodec.policy
    * W mediaswcodec: Could not read additional policy file '/vendor/etc/seccomp_policy/mediaswcodec.policy'
    
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: I1efad9cd2dfca2ba606ac0adaef71fb86b6ea6db

--- common.mk
+++ common.mk
@@ -410,7 +410,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
