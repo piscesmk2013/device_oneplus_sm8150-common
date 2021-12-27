9858c68a88aa7f4b307afc8dbec6ad135018d53d
dev-harsh1998 <harshit@hyperteam.co.in>
2021-12-27 17:50:24 +0100

    sm8150-common: Enable HWUI_COMPILE_FOR_PERF
    * activates few optimizing compiler flags for HWUI
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: c79 <vexed@riseup.net>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: Id4ef4b090d738cffa6b6af1a60c38323a639d91c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,6 +117,9 @@ DEVICE_MATRIX_FILE += $(VENDOR_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(VENDOR_PATH)/manifest.xml
 ODM_MANIFEST_FILES += $(VENDOR_PATH)/manifest-qva.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
 TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
