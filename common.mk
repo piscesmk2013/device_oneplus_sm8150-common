8964520ae94b1489615cecbc11232fdec8a9fc9b
piscesmk2013 <piscesmk2013@gmail.com>
2021-12-30 23:45:10 +0800

    Revert "Revert "sm8150-common: Build OnePlusCamera and OnePlusGallery""
    This reverts commit b0e084ca58c77f76b208fa5d61b1bbe05c187111.

--- common.mk
+++ common.mk
@@ -314,6 +314,9 @@ PRODUCT_PACKAGES += \
     Tag \
     android.hardware.nfc@1.2-service
 
+# OnePlus apps
+$(call inherit-product, vendor/oneplus/apps/config.mk)
+
 # OMX
 PRODUCT_PACKAGES += \
     libavservices_minijail \
