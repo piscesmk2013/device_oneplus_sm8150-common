d0bfe42c892cc2fe3478463472822298642a424b
piscesmk2013 <piscesmk2013@gmail.com>
2022-01-24 23:52:23 +0800

    Revert "sm8150-common: Build qualcomm-hidl from source"
    This reverts commit f89b56a29b5edb171987997f8545d61a94bd6419.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,7 @@ AB_OTA_PARTITIONS += \
     vbmeta
 
 # ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
