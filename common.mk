3dabe68a5dc07377a963f1956ea9b8d06f77db9c
timocapa <timo.capa@gmail.com>
2022-06-16 22:59:46 +0200

    common: Enable incrementalfs
    https://source.android.google.cn/devices/architecture/kernel/incfs

--- common.mk
+++ common.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Enable incrementalfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.incremental.enable=yes
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
