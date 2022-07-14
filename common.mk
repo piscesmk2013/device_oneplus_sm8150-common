4a87ccf82ee1fa674a097bc77242a74f87ada28e
Master <mhmdeveloperofficial@gmail.com>
2022-07-14 15:07:52 +0000

    sm8150-common: Add Support for OnePlusCamera [Squashed]
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/5ba732bce2cce35905a77e9ace92224a98746a50
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/729da8381cc02bb1d66a6ac4a2e5919dc7e12c75
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/309660e687c0b3420f8f551a1a06e231398d3feb
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/8eefeac1d2844b7c406366d7e1d6ef439da59c9b
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/d9af998b174ba37eda6eb0cf3112531824598571
    https://github.com/PixelExperience-Devices/device_oneplus_sm8150-common/commit/10a539d40ff2c803cef826e151b856804632a00e
    
    Co-Authored-By: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-Authored-By: gwolf2u <gwolf2u.website@gmail.com>
    Co-Authored-By: mvaisakh <mvaisakh@statixos.com>
    Co-Authored-By: mvaisakh <timo.capa@gmail.com>
    Co-Authored-By: classified <razorpaul@gmail.com>

--- common.mk
+++ common.mk
@@ -174,6 +174,7 @@ PRODUCT_PACKAGES += \
     libxml2 \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor:64 \
     vendor.oneplus.hardware.camera@1.0.vendor:64 \
+    vendor.oneplus.hardware.CameraMDMHIDL@1.0.vendor:64 \
     vendor.qti.hardware.camera.device@1.0.vendor:64
 
 # Charge control
