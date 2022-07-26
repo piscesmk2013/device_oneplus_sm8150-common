64e1e23104375b88d7a3c9d3f534b63c9c1bf1dd
piscesmk2013 <piscesmk2013@gmail.com>
2022-07-26 14:45:52 +0800

    sm8150-common: Update blobs from 11.0.9.1 and decommonize some firmware
    These firmwares are not consistent across 7Pro/7Tpro devices

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,7 +148,7 @@ CUSTOM_APNS_FILE := $(VENDOR_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-05-01
+VENDOR_SECURITY_PATCH := 2022-06-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
