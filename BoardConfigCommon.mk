af45fc812904e82e4f5c8c10d9834fe65694316a
piscesmk2013 <piscesmk2013@gmail.com>
2022-10-17 00:50:53 +0800

    sm8150-common: Update security patch level 22-08-05

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,7 +146,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-06-01
+VENDOR_SECURITY_PATCH := 2022-08-05
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
