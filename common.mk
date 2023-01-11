b7565939e159ee9167d67d3977af7ff18f304444
piscesmk2013 <piscesmk2013@gmail.com>
2023-01-11 19:19:16 +0800

    Revert "sm8150-common: Switch BtAudio to AIDL"
    This reverts commit 3652a6a0d359b5c854192d21e928e540e707f8f1.

--- common.mk
+++ common.mk
@@ -164,6 +164,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     liba2dpoffload \
+    android.hardware.bluetooth.audio@2.0:32 \
+    android.hardware.bluetooth.audio@2.0-impl:32 \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
