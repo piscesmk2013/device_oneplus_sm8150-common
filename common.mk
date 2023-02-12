ae618e11fef97e2d0c17666f7ce6c4a0ff9924d6
piscesmk2013 <piscesmk2013@gmail.com>
2023-02-12 23:26:00 +0800

    Revert "sm8150-common: Updated media codecs from OOS 12"
    This reverts commit 3adfd52e6785a0b4129e21be3e5cf7c4876bbf6c.

--- common.mk
+++ common.mk
@@ -297,7 +297,6 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/configs/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
