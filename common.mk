75781799d60dfe262fef3fe540dbdf054a35503b
John Galt <johngaltfirstrun@gmail.com>
2023-02-17 13:36:28 -0500

    sm8150-common: overlay-aosp: enable clo boostfwk

--- common.mk
+++ common.mk
@@ -8,6 +8,10 @@
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Get Perf aspects
+$(call inherit-product, device/qcom/common/system/perf/qti-perf.mk)
+$(call inherit-product, device/qcom/common/vendor/perf-legacy/qti-perf-legacy.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
