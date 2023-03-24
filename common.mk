4541ced2a35c4826ac9572a9f25f43bbe33f21a6
piscesmk2013 <piscesmk2013@gmail.com>
2023-03-25 02:30:16 +0800

    sm8150-common: Fix some errors about bluetooth in log

--- common.mk
+++ common.mk
@@ -169,8 +169,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
-    android.hardware.bluetooth.audio-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
