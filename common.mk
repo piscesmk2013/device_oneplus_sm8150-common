22b8eedd0203f52ebd4ff61f74cd8452253433d4
piscesmk2013 <piscesmk2013@gmail.com>
2023-06-07 21:07:06 +0800

    sm8150-common: Update Audio HIDL to 7.1

--- common.mk
+++ common.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.primary.default \
