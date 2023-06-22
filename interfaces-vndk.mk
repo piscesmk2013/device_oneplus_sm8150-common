d2089f8317d7d8d8c17eccd6b0e9176127feaad1
snnbyyds <snnbyyds@gmail.com>
2023-06-22 10:14:33 +0800

    sm8150-common: Update Telephony related stuffs
    Update system blobs from LA.QSSI.13.0.r1-10000.02-qssi.0
    Kang some configuration from device_qcom_common
    
    Based on https://github.com/PixelExperience-Devices/device_xiaomi_beryllium/commit/92fa189c2aa8bbdba914b56cbb925480dd374348
    
    Co-authored-by: resist15 <sourav@project404.xyz>

--- interfaces-vndk.mk
+++ interfaces-vndk.mk
@@ -34,8 +34,11 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.6 \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3 \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor
 
 # Secure Element
