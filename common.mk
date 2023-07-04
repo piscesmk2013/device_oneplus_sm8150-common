e4d4e52abccd90be42997f166db3127b12928f36
piscesmk2013 <piscesmk2013@gmail.com>
2023-07-04 21:11:27 +0800

    Revert "sm8150-common: add a rro to disable telephony injection"
    This reverts commit e15b0090bf3581088ae1de5a6aad5d326ac0bc98.

--- common.mk
+++ common.mk
@@ -442,8 +442,7 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext \
-    TelephonyResOneplus
+    telephony-ext
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
