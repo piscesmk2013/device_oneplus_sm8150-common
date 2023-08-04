c23721ec2a6e4536d55a42b3e896e2f411ce1b7a
piscesmk2013 <piscesmk2013@gmail.com>
2023-08-05 01:01:46 +0800

    sm8150-common: Build android.hardware.bluetooth.audio-impl

--- common.mk
+++ common.mk
@@ -171,7 +171,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
