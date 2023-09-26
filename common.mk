b752037f07e6bf2ecb179d27aecce18c18efef53
piscesmk2013 <piscesmk2013@gmail.com>
2023-09-26 08:13:35 +0800

    sm8150-common: Update bt params

--- common.mk
+++ common.mk
@@ -176,10 +176,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-# Bluetooth LDAC
-PRODUCT_PACKAGES += \
-    libldacBT_bco
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
