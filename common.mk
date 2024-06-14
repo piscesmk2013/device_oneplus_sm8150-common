8ee4983d10d7ec6521852e2dfdbbde9d74a84b4e
John Galt <johngaltfirstrun@gmail.com>
2024-06-14 12:52:11 -0400

    common: explicitly build some vndk vendor libraries

--- common.mk
+++ common.mk
@@ -177,6 +177,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
+    libldacBT_bco \
     android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor:32 \
     vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
@@ -373,7 +374,8 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # OnePlus
 PRODUCT_PACKAGES += \
@@ -477,6 +479,15 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oneplus
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libutilscallstack.vendor:64 \
+    libnetutils.vendor:64 \
+    libsqlite.vendor:64 \
+    libprocessgroup.vendor \
+    libjsoncpp.vendor \
+    libcurl.vendor \
+    libpng.vendor:32
+
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # Wifi
