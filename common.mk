084a82cbdcd9a19991136f12e7a0ff41606fc1a7
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    sm8150-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- common.mk
+++ common.mk
@@ -44,6 +44,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
